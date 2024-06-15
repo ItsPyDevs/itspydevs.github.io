d5b0c5bd1769130506b9116c2c6777a1ce3286f2
CrafterHide-Dev <127605336+CrafterHide-Dev@users.noreply.github.com>
2024-06-16 00:57:14 +0200

    Create goto.js

---
+++ assets/js/goto.js
@@ -0,0 +1,10 @@
+function goto(page) {
+	$("#page-content").load("/pages/"+page+".html", function(responseTxt, statusTxt, jqXHR){
+		if(statusTxt == "success"){
+			console.log("Page "+page+" loaded successfully");
+		}
+		if(statusTxt == "error"){
+			console.log("Error: [" + jqXHR.status + "] " + jqXHR.statusText);
+		}
+	});
+}

b5d91aa457e5f8cabe68e4530f395256c820590c
CrafterHide-Dev <127605336+CrafterHide-Dev@users.noreply.github.com>
2024-06-17 16:22:27 +0200

    Update text-animation.js

--- assets/js/text-animation.js
+++ assets/js/text-animation.js
@@ -1,1 +1,16 @@
-var titleElement = document.getElementById('title-anim')
+class AnimText {
+	constructor (textId="title-anim", texts=["ItsPyDevs,"], ms=100) {
+		this.textId = textId;
+		this.texts = texts;
+		this.time = ms;
+		this.on = false;
+	}
+
+	start() {
+
+	}
+
+	playAnimation () {
+
+	}
+}

4e6614ce78eee0fc8391a9b87cbbcc48b544d705
CrafterHide-Dev <127605336+CrafterHide-Dev@users.noreply.github.com>
2024-06-17 20:33:05 +0200

    Update text-animation.js

--- assets/js/text-animation.js
+++ assets/js/text-animation.js
@@ -1,16 +1,29 @@
 class AnimText {
-	constructor (textId="title-anim", texts=["ItsPyDevs,"], ms=100) {
+	constructor (textId="title-anim", texts=["ItsPyDevs, Le site.", "Front-End", "Python", "etc..."], ms=100) {
 		this.textId = textId;
 		this.texts = texts;
 		this.time = ms;
 		this.on = false;
 	}
 
-	start() {
-
+	start () {
+		this.on = true;
+		this.playAnimation();
 	}
 
 	playAnimation () {
+		while (this.on) {
+			for (i = 0; i >= this.texts.length; i++) {
+				if (!this.on) break;
+				let chars = this.texts[i].split('');
+				for (j = 0; j >= chars.length; j++) {
+					
+				}
+			}
+		}
+	}
 
+	isPlaying () {
+		return this.on;
 	}
 }
